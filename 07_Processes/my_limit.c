0317a405b9be82ad65698b1706e119c7c553432d
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-03-11 20:35:49 +0100

    Add example with limits

---
+++ 07_Processes/my_limit.c
@@ -0,0 +1,64 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+static void print_lim(rlim_t const * const lim) {
+    if (*lim == RLIM_INFINITY) {
+        printf("kein Limit\n");
+    } else {
+        printf("%lld \n", *lim);
+    }
+}
+
+static void limits(int resource) {
+    struct rlimit l;
+    
+    if (getrlimit(resource, &l) < 0) {
+        printf("Keine Angaben\n");
+        return;
+    }
+    
+    printf("Soft-Limit: ");
+    print_lim(&l.rlim_cur);
+    
+    printf("Hard-Limit: ");
+    print_lim(&l.rlim_max);
+    
+    
+}
+
+
+struct limit_and_name_t { int lim; const char * name;};
+#define LIM(lim) {lim, #lim}
+
+static const struct limit_and_name_t all_limits[] = {
+    LIM(RLIMIT_CORE),
+    LIM(RLIMIT_CPU),
+    LIM(RLIMIT_DATA),
+    LIM(RLIMIT_FSIZE),
+    LIM(RLIMIT_MEMLOCK),
+    LIM(RLIMIT_NOFILE),
+    LIM(RLIMIT_NPROC),
+    LIM(RLIMIT_RSS),
+    LIM(RLIMIT_STACK),
+#ifdef __linux__
+    LIM(RLIMIT_AS),
+    LIM(RLIMIT_MSGQUEUE),
+    LIM(RLIMIT_NICE),
+    LIM(RLIMIT_RTPRIO),
+    LIM(RLIMIT_RTTIME),
+    LIM(RLIMIT_SIGPENDING
+#endif /* __linux__ */
+};
+
+int main(void) {
+    unsigned int i;
+    for (i = 0; i < sizeof(all_limits)/sizeof(*all_limits); ++i) {
+        printf("\n%s :\n", all_limits[i].name);
+        limits(all_limits[i].lim);
+    }
+    
+    return EXIT_SUCCESS;
+}
