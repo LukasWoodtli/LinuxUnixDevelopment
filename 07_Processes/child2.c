da0f72f22b2cf3d6cdc759e9ab0c482607898b8e
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-03-12 10:28:25 +0100

    Add another example for fork

---
+++ 07_Processes/child2.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static int global_var = 1;
+
+int main(void) {
+    pid_t pid;
+    int lokal_var = 1;
+    
+    switch (pid = fork()) {
+        case -1:
+            printf("Fehler bei fork()\n");
+            break;
+        
+        case 0:
+            sleep(1);
+            
+            printf("--- Im Kindprocess ---\n");
+            printf("global_var = %d; Speicheradresse: %p\n", global_var, &global_var);
+            printf("lokal_var  = %d; Speicheradresse: %p\n", lokal_var, &lokal_var);
+            fflush(stdout);
+            
+            ++global_var;
+            ++lokal_var;
+            
+            printf("--- Im Kindprocess ---\n");
+            printf("global_var = %d; Speicheradresse: %p\n", global_var, &global_var);
+            printf("lokal_var  = %d; Speicheradresse: %p\n", lokal_var, &lokal_var);
+            fflush(stdout);
+            
+            break;
+        
+        default:
+            printf("--- Im Elternprozess ---\n");
+            printf("global_var = %d; Speicheradresse: %p\n", global_var, &global_var);
+            printf("lokal_var  = %d; Speicheradresse: %p\n", lokal_var, &lokal_var);
+            fflush(stdout);
+            
+            sleep(2);
+            
+            printf("--- Im Elternprozess ---\n");
+            printf("global_var = %d; Speicheradresse: %p\n", global_var, &global_var);
+            printf("lokal_var  = %d; Speicheradresse: %p\n", lokal_var, &lokal_var);
+            fflush(stdout);
+            
+            break;
+    }
+    
+    return EXIT_SUCCESS;
+}
+
