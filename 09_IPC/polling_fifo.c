6fc25835a5f8ceaaebfc075052c36ad8e71c28f2
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-05-17 14:40:17 +0200

    Add example for polling on FIFO

---
+++ 09_IPC/polling_fifo.c
@@ -0,0 +1,56 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define BUF 4096
+
+static void an_den_drucker(const char *text) {
+	FILE *p;
+	
+	p = popen ("lpr", "w");
+	
+	if (p == NULL) {
+		perror("popen()");
+		exit(EXIT_FAILURE);
+	}
+	
+	printf("Sende Auftrag an den Drucker\n");
+	fprintf(p, "%s", text);
+	pclose(p);
+}
+
+int main(void) {
+	char puffer[BUF], inhalt[BUF];
+	int fd;
+	inhalt[0] = '\0';
+	
+	umask(0);
+	if (mkfifo("fifo.polling", 0666) < 0) {
+		if (errno == EEXIST)
+			printf("Versuche vorhandene FIFO zu verwenden.\n");
+		else {
+			perror("mkfifo()");
+			exit(EXIT_FAILURE);
+		}
+	}
+	
+	fd = open("fifo.polling", O_RDONLY);
+	if (fd == -1) {
+		perror("open()");
+		exit(EXIT_FAILURE);
+	}
+	
+	while(1) {
+		if (read(fd, puffer, BUF))
+			an_den_drucker(puffer);
+		sleep(2);
+	}
+	
+	return EXIT_SUCCESS;
+}
+
