4776866fbf67fe80a09fb0600441437f0acc07a2
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-06-19 17:45:52 +0200

    Add example for pthread_once

---
+++ 10_Threads/thread19.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#define MAX_THREADS 3 
+
+static pthread_once_t once = PTHREAD_ONCE_INIT;
+
+void thread_once(void) {
+	printf("Funktion thread_once aufgerufen\n");
+}
+
+
+void *thread_func(void* args) {
+	(void)args;
+	printf("Thread gestartet (ID:%ld)\n", (long)pthread_self());
+	pthread_once(&once, thread_once);
+	
+	printf("Thread fertig gestartet (ID:%ld)\n\n", (long)pthread_self());
+
+	pthread_exit(NULL);
+}
+
+
+int main(void) {
+	int i;
+
+
+	pthread_t threads[MAX_THREADS];
+	
+	printf("\n-> Main-Thread gestartet (ID:%ld)\n", (long)pthread_self());
+	
+	for (i = 0; i < MAX_THREADS; ++i) 
+		pthread_create(&threads[i], NULL, (void*)thread_func, NULL);
+
+	for (i = 0; i < MAX_THREADS; ++i) 
+		pthread_join(threads[i], NULL);
+	
+	printf("<- Main-Thread beendet (ID:%ld)\n", (long)pthread_self());
+
+	return EXIT_SUCCESS;
+}
+
