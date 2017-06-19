6bb73c49880612b5f51bea9ca32e6bb894e2f0a1
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-06-19 09:21:32 +0200

    Fix indentation

--- 10_Threads/thread15.c
+++ 10_Threads/thread15.c
@@ -7,54 +7,55 @@
 
 static void cancel_test(void* arg) {
 	(void)arg;
-    int oldstate;
-    
-    if ((pthread_setcancelstate(PTHREAD_CANCEL_DISABLE,
-                                &oldstate)) !=0) {
-        printf("Fehler bei pthread_setcancelstate()\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    printf("Thread %ld im kritischen Abschnitt\n", (long)pthread_self());
-    
+	int oldstate;
+
+	if ((pthread_setcancelstate(PTHREAD_CANCEL_DISABLE,
+					&oldstate)) !=0) {
+		printf("Fehler bei pthread_setcancelstate()\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Thread %ld im kritischen Abschnitt\n", (long)pthread_self());
+
 	sleep(5);
 	
-    
-    if ((pthread_setcancelstate(oldstate, NULL)) !=0) {
-        printf("Fehler bei pthread_setcancelstate()\n");
-        exit(EXIT_FAILURE);
-    }
+
+	if ((pthread_setcancelstate(oldstate, NULL)) !=0) {
+		printf("Fehler bei pthread_setcancelstate()\n");
+		exit(EXIT_FAILURE);
+	}
 
 	printf("\t->Thread %ld nach kritischem Abschnitt\n",
-	       (long)pthread_self());
+		(long)pthread_self());
+
 	pthread_exit((void*)0);
 }
 
 
 int main(void) {
 
-    pthread_t t1;
-    long abbruch;
-    
+	pthread_t t1;
+	long abbruch;
+
 	printf("\n-> Main-Thread gestartet (ID:%ld)\n", (long)pthread_self());
 
-    if (pthread_create(&t1, NULL, (void*)cancel_test, NULL) != 0) {
-        printf("Fehler bei pthread_setcancelstate()\n");
-        exit(EXIT_FAILURE);
-    }
+	if (pthread_create(&t1, NULL, (void*)cancel_test, NULL) != 0) {
+		printf("Fehler bei pthread_setcancelstate()\n");
+		exit(EXIT_FAILURE);
+	}
 
-    pthread_cancel(t1);
-    
+	pthread_cancel(t1);
+
 	pthread_join(t1, (void*)&abbruch);
 
-    if (abbruch == (long)PTHREAD_CANCELED) {
-        printf("Fehler bei pthread_setcancelstate()\n");
-        exit(EXIT_FAILURE);
-    }
+	if (abbruch == (long)PTHREAD_CANCELED) {
+		printf("Fehler bei pthread_setcancelstate()\n");
+		exit(EXIT_FAILURE);
+	}
 
-
+	
 	printf("<- Main-Thread beendet (ID:%ld)\n", (long)pthread_self());
-    
+
 	return EXIT_SUCCESS;
 }
 
