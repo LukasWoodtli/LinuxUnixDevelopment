647dfeadf12f1f88df807a2f4cfe52c374a79354
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-06-28 12:07:54 +0200

    Add example for terminal infos and controll strings
    And fix

--- 13_Terminal/terminfo2.c
+++ 13_Terminal/terminfo2.c
@@ -6,6 +6,21 @@
 
 #define CAPS 4
 
+static void clrscr(void) {
+	char *ptr = tigetstr("clear");
+	putp(ptr);
+}
+
+static void mvcursor(int zeile, int spalte) {
+	char *cap = tigetstr("cup");
+	putp(tparm(cap, zeile, spalte));
+}
+
+static void myflash(void) {
+	char* flash = tigetstr("flash");
+	putp(flash);
+	putp(flash);
+}
 
 int main(void) {
 	int i, ret = 0;
@@ -20,34 +35,51 @@ int main(void) {
 		perror("setupterm()");
 		exit(EXIT_FAILURE);
 	}
+	
+	clrscr();
 
 	for (i = 0; i < CAPS; ++i) {
 		ret = tigetflag(boolcaps[i]);
+		mvcursor(i + 2, 10);
 		if (ret == -1)
 			printf("%s wird nicht unterstuetzt\n", boolcaps[i]);
 		else
 			printf("%s wird unterstuetzt\n", boolcaps[i]);
 	}
 	
-	printf("\n");
+	mvcursor(10, 10);
+	printf("Weiter mit ENTER\n");
+	getchar();
+	clrscr();
 	
 	for (i = 0; i < CAPS; ++i) {
 		ret = tigetnum(numcaps[i]);
+		mvcursor(i + 2, 10);
 		if (ret == -1)
 			printf("%s wird nicht unterstuetzt\n", numcaps[i]);
 		else
 			printf("%s ist %d\n", numcaps[i], ret);
 	}
-	
-	printf("\n");
 
+	mvcursor(10, 10);
+	printf("Weiter mit ENTER\n");
+	getchar();
+	clrscr();
+	
 	for (i = 0; i < CAPS; ++i) {
 		buf = tigetstr(strcaps[i]);
+		mvcursor(i + 2, 10);
 		if (buf == NULL)
 			printf("%s wird nicht unterstuetzt\n", strcaps[i]);
 		else
 			printf("%s ist \\E%s\n", strcaps[i], &buf[1]);
 	}
+
+	mvcursor(10, 10);
+	printf("Weiter mit ENTER\n");
+	getchar();
+	myflash();
+	clrscr();
 
 	return EXIT_SUCCESS;
 }
