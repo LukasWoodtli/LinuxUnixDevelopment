412d89bd497706353de0127d6ef011d79676cdad
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-07-04 11:20:10 +0200

    Fix with umlauts

--- 13_Terminal/cur6.c
+++ 13_Terminal/cur6.c
@@ -3,12 +3,12 @@
 int main (void) {
     initscr();
 
-    mvprintw(5, 5, "Diese Zeile enthält einen Fehhler");
-    mvprintw(6, 5, "Taste drücken für Korrektur");
+    mvprintw(5, 5, "Diese Zeile enthaelt einen Fehhler");
+    mvprintw(6, 5, "Taste druecken fuer Korrektur");
     getch();
     
-    mvdelch(5, 33);
-    mvprintw(7, 5, "Fehler wurde korrigiert! Bitte Taste drücken!");
+    mvdelch(5, 34);
+    mvprintw(7, 5, "Fehler wurde korrigiert! Bitte Taste druecken!");
     getch();
     
     endwin();
