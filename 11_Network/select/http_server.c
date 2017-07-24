152c34f60936dbff834f1bb6ab91898856228706
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-07-24 21:58:39 +0200

    Some more work on HTTP server

--- 11_Network/select/http_server.c
+++ 11_Network/select/http_server.c
@@ -126,7 +126,103 @@ static void SendHTTPHeader(int soc, int code, const char *phase,
 }
 
 
-
+static int httpd(int soc) {
+    int retval = 0;
+    char buf[512] = {0};
+    char method[255] = {0};
+    char uri[512] = {0};
+    char headline[1024] = {0};
+    char request_header[BUFFER_SIZE] = {0};
+    char *r = NULL;
+    char hTitle[32] = {0};
+    char hValue[128] = {0};
+    FILE *fd;
+    bool isFirst = true;
+    
+    fd = fdopen(soc, "r");
+    printf("-----------Request-Header Client-------------\n");
+    
+    while (1) {
+        r = fgets(buf, BUFFER_SIZE, fd);
+        if (*r == '\r' && *(r + 1) == '\n') {
+            break;
+        }
+        
+        printf("ClientRequest: %s", r);
+        
+        if (isFirst) {
+            strcpy(headline, r);
+            GetToken(headline, 1, method, 255, 0);
+            isFirst = false;
+        }
+        else {
+            strcat(request_header, r);
+            GetToken(r, 1, hTitle, 32, ':');
+            if (strcmp(hTitle, "Connection") == 0) {
+                GetToken(r, 2, hValue, 128, ' ');
+                if (strcmp(hValue, "Keep-Alive") == 0) {
+                    printf("Client benoetigt keep alive\r\n");
+                    retval = 1;
+                }
+                else {
+                    printf("Client benoetigt kein keep alive\r\n");
+                    retval = -1;
+                }
+            }
+        }
+    }
+    
+    printf("Fertig mit dem Lesen des Request-Headers...\r\n");
+    printf("-------------------------------------------\n");
+    fflush(stdout);
+    
+    if ((strcmp(method, "GET") == 0) || (strcmp(method, "get") == 0)) {
+        char dir[255] = {0};
+        char path[512] = {0};
+        FILE* file;
+        struct stat fst;
+        GetToken(headline, 2, uri, 1024, 0);
+        GetToken(uri, 1, dir, 255, '/');
+        printf("Folgende Datei wurde vom Client angefordert: %s\r\n", uri);
+        sprintf(path, ".%s", uri);
+        file = fopen(path, "r");
+        
+        if (file == NULL) {
+            unsigned char error404[] =
+                "<HTML><HEAD><TITLE>404 Not Found</TITLE>"
+                "</HEAD><BODY><H1>Nicht gefunden</H1>"
+                "Die angeforderte URL konnte nicht auf "
+                "disem Server gefunden werden."
+                "<P><HR><I>http_server0.1</I></BODY></HTML>\r\n\r\n";
+            printf("404 Nacricht\r\n");
+            SendHTTPHeader(soc, 404, "FILE NOT FOUND.",
+                           strlen((const char*)error404) + 1, NULL);
+            SendBlock(soc, error404, strlen((const char*)error404) + 1);
+            fflush(stdout);
+            SendBuf(soc, "\r\n\r\n");
+            return -1;
+        }
+        else {
+            unsigned char *blob = NULL;
+            stat(path, &fst);
+            printf("Datei existiert ...\r\n");
+            fflush(stdout);
+            
+            SendHTTPHeader(soc, 200, "OK", fst.st_size, &(fst.st_mtime));
+            blob = (unsigned char*)malloc(fst.st_size);
+            fread(blob, fst.st_size, 1, file);
+            SendBlock(soc, blob, fst.st_size);
+            
+            fclose(file);
+            printf("Alle Daten gesendet ...\r\n");
+            fflush(stdout);
+            SendBuf(soc, "\r\n\r\n");
+            free(blob);
+        }
+    }
+    
+    return retval;
+}
 
 
 int main(void) {
